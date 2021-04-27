99ec2848447c83cef7da647c0d8ca88c2d437029
zeliboba72 <snailman7297@gmail.com>
2021-04-27 16:43:21 +0500

    add functions for sliders

--- js/slider.js
+++ js/slider.js
@@ -2,10 +2,11 @@ const slider1 = document.getElementById("slider-1");
 const slider2 = document.getElementById("slider-2");
 
 noUiSlider.create(slider1, {
-  start: [519912, 3825000],
+  start: [500000, 3825000],
   connect: true,
+  step: 1,
   range: {
-    min: 519912,
+    min: 500000,
     max: 3825000,
   },
 });
@@ -18,4 +19,24 @@ noUiSlider.create(slider2, {
     min: [5],
     max: [15],
   },
+});
+
+const minOfFirstSlider = document.querySelector(".js-min-first");
+const maxOfFirstSlider = document.querySelector(".js-max-first");
+
+slider1.noUiSlider.on("update", function (values) {
+  minOfFirstSlider.innerHTML = `${Math.round(values[0])
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")} Р`;
+  maxOfFirstSlider.innerHTML = `${Math.round(values[1])
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")} Р`;
+});
+
+const minOfSecondSlider = document.querySelector(".js-min-second");
+const maxOfSecondSlider = document.querySelector(".js-max-second");
+
+slider2.noUiSlider.on("update", function (values) {
+  minOfSecondSlider.innerHTML = `${Math.round(values[0])} соток`;
+  maxOfSecondSlider.innerHTML = `${Math.round(values[1])} соток`;
 });
